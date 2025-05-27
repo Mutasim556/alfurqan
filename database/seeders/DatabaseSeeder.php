a040b3307d73d0f4b047bc3453a1120650e10b98
Mutasim556 <134157496+Mutasim556@users.noreply.github.com>
2025-05-27 15:43:48 +0600

    sdf

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -28,8 +28,8 @@ class DatabaseSeeder extends Seeder
             // $admin->password = Hash::make('admin');
             // $admin->save();
 
-        // $this->call(PermissionSeeder::class);
-        $this->call(RoleSeeder::class);
-        $this->call(AdminSeeder::class);
+        $this->call(PermissionSeeder::class);
+        // $this->call(RoleSeeder::class);
+        // $this->call(AdminSeeder::class);
     }
 }
